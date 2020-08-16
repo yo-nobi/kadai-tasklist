7172ea5ee3889903609674bab84db67518d739e1
takayuki.suzuki <babibubebown1018@gmail.com>
2020-08-16 12:41:35 +0000

    syuusei

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
   root to: 'tasks#index'
   
   get 'login', to: 'sessions#new'
@@ -12,7 +9,5 @@ Rails.application.routes.draw do
   resources :tasks
   
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create]
-  
-  
+  resources :users, only: [:new, :create]
 end
