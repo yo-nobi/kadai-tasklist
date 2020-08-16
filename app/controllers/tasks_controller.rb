529e6676693b8727542ed7c15f37af35d9fdb449
takayuki.suzuki <babibubebown1018@gmail.com>
2020-08-16 05:30:48 +0000

    tasklist_13

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,55 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  def index
+    @tasks = Task.order(id: :desc).page(params[:page]).per(3)
+  end
+  
+  def show
+  end
+  
+  def new
+    @task = Task.new
+  end
+  
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      flash[:success] = '投稿されました。'
+      redirect_to @task
+    else
+      flash.now[:danger] = '投稿に失敗しました。'
+      render :new
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @task.update(task_params)
+      flash[:success] = '更新しました。'
+      redirect_to @task
+    else
+      flash.now[:danger] = '更新されませんでした。'
+      render :edit
+    end
+  end
+  
+  def destroy
+    @task.destroy
+
+    flash[:success] = '正常に削除されました'
+    redirect_to tasks_url
+  end
+  
+  private
+  
+  def set_task
+    @task = Task.find(params[:id])
+  end
+  
+  def task_params
+    params.require(:task).permit(:content, :status)
+  end
+      
+end
