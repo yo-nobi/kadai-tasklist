451f26f1da0ebb4f78dc347b0c902afb2503c39c
takayuki.suzuki <babibubebown1018@gmail.com>
2020-08-17 12:38:45 +0000

    syuusei5

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in 
-  before_action :correct_user, only: [:show, :edit, :destroy]
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   
   def index
@@ -48,10 +47,6 @@ class TasksController < ApplicationController
   end
   
   private
-  
-  def set_task
-    @task = Task.find(params[:id])
-  end
   
   def task_params
     params.require(:task).permit(:content, :status)
