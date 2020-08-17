e4d6765489f86020974717811359f208f60c08eb
takayuki.suzuki <babibubebown1018@gmail.com>
2020-08-17 09:27:05 +0000

    syusei2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,20 +1,23 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  before_action :require_user_logged_in, only: [:index, :show, :new, :edit]
+  before_action :require_user_logged_in
   
   def index
-    @tasks = Task.order(id: :desc).page(params[:page]).per(3)
+    if logged_in?
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page]).per(3)
+    end
   end
   
   def show
+    @tasks = current_user.tasks.order(id: :desc).page(params[:page])
   end
   
   def new
-    @task = Task.new
+    @task = current_user.tasks.build
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = '投稿されました。'
       redirect_to @task
